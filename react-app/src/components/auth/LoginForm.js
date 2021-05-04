2d2f292425a5d4fd2007cc936ec71c6d49618445
Savannah Rice <savannah.rice@gmail.com>
2021-05-04 17:15:29 -0600

    Added some styling to login

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -3,6 +3,7 @@ import  { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 import { login } from "../../store/session";
 import styles from './LoginForm.module.css'
+import photo from './coen-van-de-broek-m3deylWrxHw-unsplash.jpg'
 
 const LoginForm = () => {
   const dispatch = useDispatch();
@@ -32,34 +33,42 @@ const LoginForm = () => {
   }
 
   return (
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error) => (
-          <div>{error}</div>
-        ))}
+    <div className={styles.entirePage}>
+      <div className={styles.leftSide}>
+        <h2>Welcome Back. Please login to you account.</h2>
+        <form onSubmit={onLogin}>
+          <div className={styles.errors}>
+            {errors.map((error) => (
+              <div>{error}</div>
+            ))}
+          </div>
+          <div className={styles.formInput}>
+            <input
+              name="email"
+              type="text"
+              placeholder="Email"
+              value={email}
+              onChange={updateEmail}
+            />
+          </div>
+          <div className={styles.formInput}>
+            <input
+              name="password"
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={updatePassword}
+        
+            />
+          </div>
+          <div className={styles.btnDiv}>
+            <button type="submit" className={styles.formBtn}>Login</button>
+            <button className={styles.formBtn}>Sign Up</button>
+          </div>
+        </form>
       </div>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input
-          name="email"
-          type="text"
-          placeholder="Email"
-          value={email}
-          onChange={updateEmail}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input
-          name="password"
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={updatePassword}
-        />
-        <button type="submit">Login</button>
-      </div>
-    </form>
+      <div className={styles.rightSide}><img src={photo} alt=""/></div>
+    </div>
   );
 };
 
